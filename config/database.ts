ed1f4f4b501a23c474491219a6ab8c5b5144b868
pizzalicer <alicerodrigueslili.05@gmail.com>
2023-09-25 09:32:01 -0300

    pronto

--- config/database.ts
+++ config/database.ts
@@ -40,10 +40,7 @@ const databaseConfig: DatabaseConfig = {
         port: Env.get('MYSQL_PORT'),
         user: Env.get('MYSQL_USER'),
         password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
-        ssl: {
-          rejectUnauthorized: false
-        }
+        database: Env.get('MYSQL_DB_NAME')
       },
       migrations: {
         naturalSort: true,
@@ -71,6 +68,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
       migrations: {
         naturalSort: true,
