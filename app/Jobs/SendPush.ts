eef04b55033cb431c24cdf02155bf5ebc981d0e6
pizzalicer <alicerodrigueslili.05@gmail.com>
2023-11-06 10:23:42 -0300

    deploy

--- app/Jobs/SendPush.ts
+++ app/Jobs/SendPush.ts
@@ -1,4 +1,6 @@
 import type { JobHandlerContract, Job } from '@ioc:Rlanz/Queue'
+import { apiAxios } from '../../config/app'
+import { AxiosError } from 'node_modules/axios'
 
 export type SendPushPayload = {}
 
@@ -10,7 +12,19 @@ export default class implements JobHandlerContract {
   /**
    * Base Entry point
    */
-  public async handle(payload: SendPushPayload) {}
+  public async handle(payload: SendPushPayload) {
+    try {
+      console.log(payload)
+      await apiAxios.post(
+        'https://exp.host/--/api/v2/push/send',
+        payload
+      );
+      console.log('foi')
+    } catch (error) {
+      const err = error as AxiosError
+      console.log(err.response?.data)
+    }
+  }
 
   /**
    * This is an optional method that gets called if it exists when the retries has exceeded and is marked failed.
